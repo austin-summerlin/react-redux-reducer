d8e02076e16386c4b387472581dc272e09e3543d
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-17 11:01:13 -0700

    add reduxProvider to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import { ReduxProvider } from './state/ReduxProvider';
+import { intialState, reducer } from './state/reducer';
 
 render(
-  <App />,
+  <ReduxProvider reducer={reducer} intialState={intialState}>
+    <App />
+  </ReduxProvider>,
   document.getElementById('root')
 );
